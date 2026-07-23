05a2f5d988d3071b88d42ca1cd38e53412f321b5
Hoàng Phú <62040675+hoangphu1607@users.noreply.github.com>
2026-07-23 14:47:54 +0700

    test

--- Snak/First/src/main.js
+++ Snak/First/src/main.js
@@ -12,8 +12,8 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: false,
-            gravity: { y: 500 }
+            gravity: { y: 300 },
+            debug: false
         }
     },
     scale: {
